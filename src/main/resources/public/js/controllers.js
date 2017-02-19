bb2f14b7fa901613d5d37201820932d1623da834
William RAD Funk <contact@WILLIAMRADFunk.com>
2017-02-19 03:47:08 -0500

    Completes functionality for createMovie and deleteMovie. Adds some styling, but Responsive still needs work.

--- src/main/resources/public/js/controllers.js
+++ src/main/resources/public/js/controllers.js
@@ -12,26 +12,41 @@ movieDirectoryApp.controller("MainController", ["mainData", function(main)
 	self.gotoGetMovie = function()
 	{
 		self.state = "getMovie";
+		main.reset();
 	}
 	self.gotoGetAllMovies = function()
 	{
 		self.state = "getAllMovies";
+		main.reset();
 		main.getMovies();
 	}
 	self.gotoCreateMovie = function()
 	{
 		self.state = "createMovie";
+		main.reset();
 	}
 	self.gotoEditMovie = function()
 	{
 		self.state = "editMovie";
+		main.reset();
 	}
 	self.gotoDeleteMovie = function()
 	{
 		self.state = "deleteMovie";
+		main.reset();
 	}
 	self.getMovieById = function(id)
 	{
+		main.reset();
 		main.getMovieById(id);
+	}
+	self.createMovie = function()
+	{
+		main.createMovie(self.movie.title, self.movie.synopsis, self.movie.optimalSeason, self.movie.worstSeason, self.movie.costLicense, self.movie.licenseLength, self.movie.producedBy);
+	}
+	self.deleteMovie = function(id)
+	{
+		main.reset();
+		main.deleteMovie(id);
 	}
 }]);
--- src/main/resources/public/js/services.js
+++ src/main/resources/public/js/services.js
@@ -21,7 +21,118 @@ movieDirectoryApp.factory('mainData', ['$http', function($http)
 	main.errors = {};
 	main.errors.getMovieError = "";
 	main.errors.getMoviesError = "";
+	main.errors.createMovieError = "";
+	main.errors.deleteMovieError = "";
 
+	// Creates a new movie from the information given by user
+	main.createMovie = function(title, synopsis, optimalSeason, worstSeason, costLicense, licenseLength, producedBy)
+	{
+		console.log(title, synopsis, optimalSeason, worstSeason, costLicense, licenseLength, producedBy);
+		if(	title === null || title === "" || title === undefined ||
+			synopsis === null || synopsis === "" || synopsis === undefined ||
+			optimalSeason === null || optimalSeason === "" || optimalSeason === undefined ||
+			worstSeason === null || worstSeason === "" || worstSeason === undefined ||
+			costLicense === null || costLicense === "" || costLicense === undefined ||
+			licenseLength === null || licenseLength === "" || licenseLength === undefined ||
+			producedBy === null || producedBy === "" || producedBy === undefined)
+		{
+			main.errors.createMovieError = "All fields must be filled out.";
+			return;
+		}
+		else if(optimalSeason < 0 || optimalSeason > 3)
+		{
+			main.errors.createMovieError = "Optimal Season must be 0, 1, 2, or 3";
+			return;
+		}
+		else if(worstSeason < 0 || worstSeason > 3)
+		{
+			main.errors.createMovieError = "Worst Season must be 0, 1, 2, or 3";
+			return;
+		}
+		else if(costLicense < 1000 || costLicense > 10000)
+		{
+			main.errors.createMovieError = "Cost of License must be between $1,000 and $10,000";
+			return;
+		}
+		else if(licenseLength < 12 || licenseLength > 52)
+		{
+			main.errors.createMovieError = "License Duration must be 12 and 52 weeks";
+			return;
+		}
+		else
+		{
+			$http(
+			{
+				method: 'POST',
+				url: '/movie',
+				data: {
+					"title": title,
+					"synopsis": synopsis,
+					"optimalSeason": optimalSeason,
+					"worstSeason": worstSeason,
+					"costLicense": costLicense,
+					"licenseLength": licenseLength,
+					"producedBy": producedBy
+				}
+			}).then(function successCallback(response)
+			{
+				console.log(response.data);
+				if(response.data !== null)
+				{
+					main.movie.id = response.data.id;
+					main.movie.title = response.data.title;
+					main.movie.synopsis = response.data.synopsis;
+					main.movie.expectedPopularity = response.data.expectedPopularity;
+					main.movie.optimalSeason = response.data.optimalSeason;
+					main.movie.worstSeason = response.data.worstSeason;
+					main.movie.costLicense = response.data.costLicense;
+					main.movie.licenseLength = response.data.licenseLength;
+					main.movie.producedBy = response.data.producedBy;
+					main.movie.dateCreated = response.data.dateCreated;
+					main.movie.dateModified = response.data.dateModified;
+				}
+				else
+				{
+					main.errors.createMovieError = "Failed to create your movie."
+				}
+			}, function errorCallback(response)
+			{
+				main.errors.createMovieError = response.statusText;
+			});
+		}
+	}
+	// Deletes movie by id and returns it to controller.
+	main.deleteMovie = function(id)
+	{
+		$http(
+			{
+				method: 'DELETE',
+				url: '/movie/' + id
+			}).then(function successCallback(response)
+			{
+				if(response.data !== null)
+				{
+					main.movie.id = response.data.id;
+					main.movie.title = response.data.title;
+					main.movie.synopsis = response.data.synopsis;
+					main.movie.expectedPopularity = response.data.expectedPopularity;
+					main.movie.optimalSeason = response.data.optimalSeason;
+					main.movie.worstSeason = response.data.worstSeason;
+					main.movie.costLicense = response.data.costLicense;
+					main.movie.licenseLength = response.data.licenseLength;
+					main.movie.producedBy = response.data.producedBy;
+					main.movie.dateCreated = response.data.dateCreated;
+					main.movie.dateModified = response.data.dateModified;
+				}
+				else
+				{
+					main.errors.deleteMovieError = "No movie found with that id."
+				}
+			}, function errorCallback(response)
+			{
+				main.errors.deleteMovieError = response.statusText;
+			});
+	}
 	// Gets movie by id and returns it to controller.
 	main.getMovieById = function(id)
 	{
@@ -75,6 +186,23 @@ movieDirectoryApp.factory('mainData', ['$http', function($http)
 			{
 				main.errors.getMoviesError = response.statusText;
 			});
+	}
+	// Reset the main.movie and main.movies content
+	main.reset = function()
+	{
+		main.movie.id = 0;
+		main.movie.title = "";
+		main.movie.synopsis = "";
+		main.movie.expectedPopularity = 0.0;
+		main.movie.optimalSeason = 0;
+		main.movie.worstSeason = 0;
+		main.movie.costLicense = 0.0;
+		main.movie.licenseLength = 0;
+		main.movie.producedBy = "";
+		main.movie.dateCreated = "";
+		main.movie.dateModified = "";
+
+		main.movies.list = [];
 	}
 
 	// Pass one-way data to those dependent on the service.

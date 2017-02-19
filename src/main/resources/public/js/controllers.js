862c0e2c129c5621c217e32129c47b8821c2acdc
William RAD Funk <contact@WILLIAMRADFunk.com>
2017-02-19 02:44:15 -0500

    Completes the angular 1 functionality for the get movie by id and get movie list. Adds some preliminary styling, but more attention needed toward Responive. Removes .classpath and .project from .gitignore since these do appear to be relevant components in reproducing the Eclipse project from scratch on new computer.

--- src/main/resources/public/js/controllers.js
+++ src/main/resources/public/js/controllers.js
@@ -1,8 +1,37 @@
 // Main function is to instigate the game timer.
-movieDirectoryApp.controller('MainController', ['mainData', function(main)
+movieDirectoryApp.controller("MainController", ["mainData", function(main)
 {
 	var self = this;
 
-	self.state = main.state;
+	self.state = "index";
+	self.errors = main.errors;
 
+	self.movie = main.movie;
+	self.movies = main.movies;
+
+	self.gotoGetMovie = function()
+	{
+		self.state = "getMovie";
+	}
+	self.gotoGetAllMovies = function()
+	{
+		self.state = "getAllMovies";
+		main.getMovies();
+	}
+	self.gotoCreateMovie = function()
+	{
+		self.state = "createMovie";
+	}
+	self.gotoEditMovie = function()
+	{
+		self.state = "editMovie";
+	}
+	self.gotoDeleteMovie = function()
+	{
+		self.state = "deleteMovie";
+	}
+	self.getMovieById = function(id)
+	{
+		main.getMovieById(id);
+	}
 }]);
--- src/main/resources/public/js/services.js
+++ src/main/resources/public/js/services.js
@@ -1,9 +1,81 @@
 movieDirectoryApp.factory('mainData', ['$http', function($http)
 {
-	var balance = 10000;
 	main = {};
-	
-	main.state = "index";
+	main.movie = {};
+	main.movie.id = 0;
+	main.movie.title;
+	main.movie.synopsis;
+	main.movie.expectedPopularity;
+	main.movie.optimalSeason;
+	main.movie.worstSeason;
+	main.movie.costLicense;
+	main.movie.licenseLength;
+	main.movie.producedBy;
+	main.movie.dateCreated;
+	main.movie.dateModified;
+
+	main.movies = {};
+	main.movies.list = [];
+
+
+	main.errors = {};
+	main.errors.getMovieError = "";
+	main.errors.getMoviesError = "";
+
+	// Gets movie by id and returns it to controller.
+	main.getMovieById = function(id)
+	{
+		$http(
+			{
+				method: 'GET',
+				url: '/movie/' + id
+			}).then(function successCallback(response)
+			{
+				if(response.data !== null)
+				{
+					main.movie.id = response.data.id;
+					main.movie.title = response.data.title;
+					main.movie.synopsis = response.data.synopsis;
+					main.movie.expectedPopularity = response.data.expectedPopularity;
+					main.movie.optimalSeason = response.data.optimalSeason;
+					main.movie.worstSeason = response.data.worstSeason;
+					main.movie.costLicense = response.data.costLicense;
+					main.movie.licenseLength = response.data.licenseLength;
+					main.movie.producedBy = response.data.producedBy;
+					main.movie.dateCreated = response.data.dateCreated;
+					main.movie.dateModified = response.data.dateModified;
+				}
+				else
+				{
+					main.errors.getMovieError = "No movie found with that id."
+				}
+			}, function errorCallback(response)
+			{
+				main.errors.getMovieError = response.statusText;
+			});
+	}
+	// Gets all movies and returns them to controller.
+	main.getMovies = function()
+	{
+		$http(
+			{
+				method: 'GET',
+				url: '/movie'
+			}).then(function successCallback(response)
+			{
+				if(response.data !== null)
+				{
+					main.movies.list = response.data;
+				}
+				else
+				{
+					main.errors.getMoviesError = "No movies found."
+				}
+			}, function errorCallback(response)
+			{
+				main.errors.getMoviesError = response.statusText;
+			});
+	}
 
 	// Pass one-way data to those dependent on the service.
 	return main;
